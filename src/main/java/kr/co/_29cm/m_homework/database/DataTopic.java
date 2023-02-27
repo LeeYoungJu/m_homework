f16b739cb0d2df55e9069051f6c52df273b0202b
yjlee <dmlqhqhqh@naver.com>
2023-02-28 01:32:21 +0900

    modify a lot of things

--- src/main/java/kr/co/_29cm/m_homework/database/DataTopic.java
+++
@@ -1,7 +0,0 @@
-package kr.co._29cm.m_homework.database;
-
-public class DataTopic {
-    public static final String PRODUCT = "product";
-    public static final String ORDER = "order";     // 추후 사용 예상
-    public static final String USER = "user";       // 추후 사용 예상
-}
